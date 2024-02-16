35219eac2a5d5e68319df7d80cbf5a4bd6a84a87
Anita Caron <anitacaron@users.noreply.github.com>
2024-02-16 14:09:56 +0000

    generate graph view

--- src/validation.py
+++ src/validation.py
@@ -1,12 +1,15 @@
 """
 Main script to run workflow for each data in config file
 """
+import argparse
 import os
 from pathlib import Path
 
 import pandas as pd
 from asct_parser.asct_parser import parse_asctb
 from asct_parser.utils.util import load_asctb
+from relation_validator.utils.utils import (get_labels, get_obograph,
+                                            save_obograph)
 from relation_validator.validator import run_validation
 
 from config.validation_config import load_validation_config
@@ -27,13 +30,30 @@ def validate(configfile: Path):
             os.mkdir(data_dir)
 
         if d.sheet_id and d.gid:
-            asct_parsed = parse_asctb(load_asctb(gid=d.gid, sheet_id=d.sheet_id))
+            asct_parsed = parse_asctb(
+                load_asctb(gid=d.gid, sheet_id=d.sheet_id)
+            )
+            temp_filename = f"{d.name}-{d.version}"
+
         if d.filename:
             asct_parsed = pd.read_csv(d.filename)
+            temp_filename = os.path.splitext(os.path.split(d.filename)[1])[0]
 
-        report = run_validation(asct_parsed, config.relationships)
+        report, rel_terms = run_validation(asct_parsed, config.relationships)
+        labels = get_labels(asct_parsed)
+        graph = get_obograph(rel_terms, labels)
+        save_obograph(graph, f"{data_dir}/{temp_filename}.png")
 
-        report.to_csv(f"{data_dir}/{d.name}-{d.version}.tsv", sep="\t", index=False)
+        report.to_csv(
+            f"{data_dir}/{d.name}-{d.version}.tsv", sep="\t", index=False
+        )
 
 
-validate("validation-config.yml")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--config", type=Path, required=True, help="yaml file"
+    )
+    args = parser.parse_args()
+
+    validate(args.config)
